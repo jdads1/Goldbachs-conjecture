65e9d9a2c1fb201c99c5108fce97a6cd29f05ed2
Joshua Dadswell <josh.dadswell@suitcasetv.com>
2019-04-04 16:53:27 +0100

    Created

---
+++ GoldbachsConjecture/PrimeHelpers.cs
@@ -0,0 +1,73 @@
+﻿namespace GoldbachsConjecture
+{
+    class PrimeHelpers
+    {
+        public static bool IsPrime(int Number)
+        {
+            bool Result = true; // assume prime until found otherwise
+
+            // start at 2 and search up to half of the number
+            // this could probably be made more efficient by only searching with primes
+            for (int i = 2; i <= (Number / 2); i++)
+            {
+                double DivResult = Number / (double)i;
+
+                // if the result does not have any fractional value then it is not a prime
+                if (DivResult - ((int)DivResult) == 0)
+                {
+                    // number is not prime
+                    Result = false;
+                    break;
+                }
+            }
+
+            return Result;
+        }
+
+        public static int FindLargestPrime(int Max)
+        {
+            int Result = -1;
+
+            // validate input
+            if (Max > 0)
+            {
+                // start search for prime at max and work down
+                for (int i = Max; i > 0; i--)
+                {
+                    if (IsPrime(i))
+                    {
+                        Result = i;
+                        break;
+                    }
+                }
+            }
+
+            return Result;
+        }
+
+        public static int FindNextPrime(int Number)
+        {
+            int Result = -1;
+
+            // validate input
+            if (Number > 0)
+            {
+                // start search at the next number
+                int currNumber = Number + 1;
+                while( Result < 0 )
+                {
+                    if(IsPrime(currNumber))
+                    {
+                        Result = currNumber;
+                    }
+                    else
+                    {
+                        currNumber++;
+                    }
+                }
+            }
+
+            return Result;
+        }
+    }
+}
--- GoldbachsConjecture/Program.cs
+++ GoldbachsConjecture/Program.cs
@@ -6,7 +6,81 @@ namespace GoldbachsConjecture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool exit = false;
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------PRIME CALCULATOR--------------------");
+            Console.WriteLine("-------------------------------------------------------");
+
+            // loop until done
+            while (!exit)
+            {
+                Console.WriteLine("Please input a numebr (type exit to quit):");
+                string userInput = Console.ReadLine();
+
+                // if the user types exit we are done
+                if (userInput.ToLower() == "exit")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    // attempt to convert the string to a number
+                    if (int.TryParse(userInput, out int userNumber))
+                    {
+                        // check if the number is prime
+                        bool isPrime = PrimeHelpers.IsPrime(userNumber);
+
+                        if (isPrime)
+                        {
+                            Console.WriteLine(userNumber + " is a prime");
+                        }
+                        else
+                        {
+                            Console.WriteLine(userNumber + " is not a prime");
+                            Console.WriteLine("Largest prime below " + userNumber + " is " + PrimeHelpers.FindLargestPrime(userNumber));
+                        }
+
+                        Console.WriteLine("Next prime is " + PrimeHelpers.FindNextPrime(userNumber));
+
+                        if (!isPrime)
+                        {
+                            int primeFactorCount = 0;
+                            int maxSearch = userNumber / 2;
+                            for( int i = 2; i < maxSearch; i = PrimeHelpers.FindNextPrime(i))
+                            {
+                                int factor1 = i; // we know this is prime
+                                double factor2 = userNumber / (double)factor1; // if this is prime we have found prime factors
+
+                                if(factor2 - ((int)factor2) == 0 )
+                                {
+                                    // factor 2 is a whole number
+                                    if(PrimeHelpers.IsPrime((int)factor2))
+                                    {
+                                        // factor 2 is a prime
+                                        Console.WriteLine("Prime Factors: " + factor1 + "x" + factor2 + "=" + userNumber);
+
+                                        primeFactorCount++;
+                                    }
+                                }
+                            }
+
+                            if(primeFactorCount == 0)
+                            {
+                                Console.WriteLine("No prime factors found");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cannot have prime factors of a prime number");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Number");
+                    }
+                }
+            }
         }
     }
 }
